eb204e862bc0ec634d51a339803710941eae9e08
ASPCODES <adityawork0811@gmail.com>
2025-10-10 20:28:48 +0530

    Update database connection log messages for consistency

--- Backend/src/db/prisma.ts
+++ Backend/src/db/prisma.ts
@@ -6,9 +6,9 @@ const prisma = new PrismaClient();
 export const connectDB = async (): Promise<void> => {
     try {
         await prisma.$connect();
-        console.log("MongoDB connected!!");
+        console.log("Database connected successfully!!");
     } catch (error: any) {
-    console.error("MongoDB connection failed:", error.message);
+    console.error("Database connection failed:", error.message);
     throw new ApiError(500, "Database connection failed");
   }
 };
